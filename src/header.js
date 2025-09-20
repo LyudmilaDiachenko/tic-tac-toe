c219a421e3824f2e27de4806fb850d7c96a2ca99
LyudmilaDiachenko <lyudmila.diachenko@gmail.com>
2025-09-20 12:40:13 +0300

    draw support

--- src/header.js
+++ src/header.js
@@ -3,8 +3,10 @@ import React from "react";
 function Header({turn, error, win}) {
     if (error){
         return <h1 className="">Невірний хід</h1>
-    } else if (win){
+    } else if (win && win !== 'draw'){
         return <h1 className="">Перемога {turn}</h1>
+    } else if (win === 'draw'){
+        return <h1 className="">Нічия</h1>
     } else {
         return <h1 className="">Хід гравця {turn}</h1>
     }
