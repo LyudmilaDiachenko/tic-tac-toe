7daed86dfe3057c151ba0bc1b86ea8d8d55442eb
LyudmilaDiachenko <lyudmila.diachenko@gmail.com>
2025-09-26 14:03:20 +0300

    sound effects

--- src/js/grid.js
+++ src/js/grid.js
@@ -23,15 +23,21 @@ function Grid({turn, setTurn, win, setWin, showError, grid, setGrid, log, setLog
 
     }
     
+    function playSound(file){
+        new Audio(`/media/${file}.mp3`).play()
+    }
+
     function onClick(row, col){
         if (!grid[row][col] && !win){
             let newGrid = [...grid]
             newGrid[row][col] = turn
             setGrid(newGrid)
+            playSound(turn)
             validateWin(newGrid) || setTurn(turn === 'x' ? 'o' : 'x')
         } else if (win) {
             // noop
         } else {
+            playSound('error')
             showError()
         }
     }
@@ -39,34 +45,43 @@ function Grid({turn, setTurn, win, setWin, showError, grid, setGrid, log, setLog
     function validateWin(grid){
         let isWin = false;
         if (grid[0][0] && grid[0][0] === grid[0][1] && grid[0][1] === grid[0][2]){
-            isWin = submitWin('win_h1')
+            isWin = gameOver('win_h1')
         } else if (grid[1][0] && grid[1][0] === grid[1][1] && grid[1][1] === grid[1][2]){
-            isWin = submitWin('win_h2')
+            isWin = gameOver('win_h2')
         } else if (grid[2][0] && grid[2][0] === grid[2][1] && grid[2][1] === grid[2][2]){
-            isWin = submitWin('win_h3')
+            isWin = gameOver('win_h3')
         } else if (grid[0][0] && grid[0][0] === grid[1][0] && grid[1][0] === grid[2][0]){
-            isWin = submitWin('win_v1')
+            isWin = gameOver('win_v1')
         } else if (grid[0][1] && grid[0][1] === grid[1][1] && grid[1][1] === grid[2][1]){
-            isWin = submitWin('win_v2')
+            isWin = gameOver('win_v2')
         } else if (grid[0][2] && grid[0][2] === grid[1][2] && grid[1][2] === grid[2][2]){
-            isWin = submitWin('win_v3')
+            isWin = gameOver('win_v3')
         } else if (grid[0][0] && grid[0][0] === grid[1][1] && grid[1][1] === grid[2][2]){
-            isWin = submitWin('win_d1')
+            isWin = gameOver('win_d1')
         } else if (grid[0][2] && grid[0][2] === grid[1][1] && grid[1][1] === grid[2][0]){
-            isWin = submitWin('win_d2')
+            isWin = gameOver('win_d2')
         } else if (
             grid[0][0] && grid[0][1] && grid[0][2] && 
             grid[1][0] && grid[1][1] && grid[1][2] && 
             grid[2][0] && grid[2][1] && grid[2][2]){
-            isWin = submitWin("draw")
+            isWin = gameOver("draw")
         }
         return isWin;
     }
 
-    function submitWin(winType){
-        setLog([...log].concat(winType === "draw" ? '' : turn))
-        setWin(winType)
+    function gameOver(gameOverType){
+        setLog([...log].concat(gameOverType === "draw" ? '' : turn))
+        setWin(gameOverType)
+        playSound(gameOverSound(gameOverType))
         return true;
+    }
+
+    function gameOverSound(gameOverType){
+        if (gameOverType === 'draw') return 'loss';
+        if (aiPlayer === turn) return 'loss';
+        if (aiPlayer === 'b') return 'win';
+        if (aiPlayer === '') return 'win';
+        return 'win';
     }
 
     useEffect(() => {
